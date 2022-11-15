8c2af69d898398d8de1fcfa091cd37c3fa7a65ee
Meraz Meraz <devmeraz87@gmail.com>
2022-11-15 22:54:21 +0600

    fa

--- js/splitText__animation.js
+++ js/splitText__animation.js
@@ -1,7 +1,51 @@
 gsap.registerPlugin(SplitText, ScrollTrigger);
 
 
-let mySplitText = new SplitText(".split", {type: "chars"});
+window.addEventListener("load", function () {
+    gsap.registerPlugin(ScrollTrigger, SplitText);
+  
+    const pageContainer = document.querySelector("._container");
+    pageContainer.setAttribute("data-scroll-container", "");
+  
+    const scroller = new LocomotiveScroll({
+      el: pageContainer,
+      inertia: 0.8,
+      smooth: true,
+      getDirection: true,
+      mobile: {
+        smooth: true
+      },
+      tablet: {
+          smooth: true
+      }
+    });
+    
+    scroller.on("scroll", function (t) {
+      document.documentElement.setAttribute("data-direction", t.direction);
+    });
+    
+    scroller.on("scroll", ScrollTrigger.update);
+    
+    ScrollTrigger.scrollerProxy(pageContainer, {
+      scrollTop(value) {
+        return arguments.length ?
+        scroller.scrollTo(value, 0, 0) :
+        scroller.scroll.instance.scroll.y;
+      },
+      getBoundingClientRect() {
+        return {
+          left: 0,
+          top: 0,
+          width: window.innerWidth,
+          height: window.innerHeight };
+  
+      },
+  
+     pinType: pageContainer.style.transform ? "transform" : "fixed" });
+
+
+
+     let mySplitText = new SplitText(".split", {type: "chars"});
 
 
 let chars = mySplitText.chars;
@@ -14,6 +58,7 @@ gsap.from(chars, {
     ease: "back.out",
     duration: 0.5,
     scrollTrigger : {
+        scroller: "[data-scroll-container]",
         trigger: ".split",
         start: "top center",
         // scrub: true, 
@@ -23,3 +68,9 @@ gsap.from(chars, {
         console.log("reverted sucessfully");
     }
 })
+
+  
+});
+
+
+
