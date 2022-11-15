dabe91b65d3ffc529f4fb104a751bbe72f64aeb0
Meraz Meraz <devmeraz87@gmail.com>
2022-11-15 23:04:59 +0600

    dfa

--- js/splitText__animation.js
+++ js/splitText__animation.js
@@ -57,7 +57,7 @@ gsap.from(chars, {
     stagger: 0.01,
     ease: "back.out",
     opacity: 0,
-    duration: 0.5,
+    duration: 1,
     scrollTrigger : {
         scroller: "[data-scroll-container",
         trigger: ".split",
